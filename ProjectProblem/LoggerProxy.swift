162f2b0bafcd9fb0d2702917c1eeeaf2ff111d55
Avi Cohen <cohena100@gmail.com>
2016-07-15 12:17:15 +0300

    log is done in the background

--- ProjectProblem/LoggerProxy.swift
+++ ProjectProblem/LoggerProxy.swift
@@ -10,22 +10,38 @@ import Foundation
 
 class LoggerProxy {
     
-}
+    var fileHandler: NSFileHandle!
+    let operationQueue = NSOperationQueue()
+    
+    init() {
+        operationQueue.name = "File Logger"
+        operationQueue.maxConcurrentOperationCount = 1
+        operationQueue.qualityOfService = .Background
 
-extension LoggerProxy: ILoggerProxy {
- 
-    func log(line: String) {
         let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
         let documentsDirectory = paths[0]
         let appFile = documentsDirectory.stringByAppendingString("/fileLog")
-        var fileHandler: NSFileHandle! = NSFileHandle(forUpdatingAtPath: appFile)
+        fileHandler = NSFileHandle(forUpdatingAtPath: appFile)
         if fileHandler == nil {
             NSFileManager.defaultManager().createFileAtPath(appFile, contents: nil, attributes: nil)
             fileHandler = NSFileHandle(forUpdatingAtPath: appFile)
         }
         fileHandler.seekToEndOfFile()
-        fileHandler.writeData((line + "\n\n").dataUsingEncoding(NSUTF8StringEncoding)!)
+    }
+    
+    deinit {
         fileHandler.closeFile()
+    }
+    
+}
+
+extension LoggerProxy: ILoggerProxy {
+ 
+    func log(line: String) {
+        operationQueue.addOperationWithBlock {
+            self.fileHandler.writeData((line + "\n\n").dataUsingEncoding(NSUTF8StringEncoding)!)
+            self.fileHandler.synchronizeFile()
+        }
     }
     
 }
