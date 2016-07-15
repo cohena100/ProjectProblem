7e2069cda953d0808b76a453e95996614d0c39c3
Avi Cohen <cohena100@gmail.com>
2016-07-15 14:08:28 +0300

    fixed the way process arguments are checked for tests

--- ProjectProblem/AppDelegate.swift
+++ ProjectProblem/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         let arguments = NSProcessInfo.processInfo().arguments
-        if arguments.count > 1 {
+        if arguments.contains("-test") {
             Model.sharedInstance.factory = ModelFactoryMock()
         } else {
             Model.sharedInstance.factory = ModelFactory()
